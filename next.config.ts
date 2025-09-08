502bfb066f108d6404e3b88e36fb99b1bf20441b
Daruma.K <k.net.game01@gmail.com>
2025-09-08 18:15:54 +0700

    update module images next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig = {
     allowedDevOrigins: [
         'http://localhost:3002',
         'http://192.168.1.163:3002',
-    ]
+    ],
+    images: {
+        qualities: [50, 75, 80, 85, 90, 95, 100],
+    },
 };
 
 module.exports = nextConfig;
